98a49e02fea157f3a17bb2ca145bffbeef5fe94c
kibruu <126585310+kibruu@users.noreply.github.com>
2026-08-06 14:20:55 +0200

    feat: add pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,20 @@ import type { ProductsResponse } from "./types";
 import Header from "./components/Header/Header";
 import SummaryCards from "./components/Summary-card/SummaryCard";
 import SearchBar from "./components/SearchBar";
+import { Pagination } from "./components/Pagination/Pagination";
 
 const API_URL = "http://localhost:4000";
 const defaultLimit = "6";
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: Promise<{
+    page?: string;
+  }>;
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const params = await searchParams;
+  const currentPage = Number(params.page ?? 1);
 
   // we use the fetch() method to get the products from the API
   // in this fetch we sort using _sort and _order and we limit the number of products using _limit
@@ -15,14 +24,18 @@ export default async function Home() {
   // we can use the other destructed variables like page, total and so on to create pagination or show info
   //fetching data from the API and destructuring the response to get the products, total, page, pages and limit
   const { products, total, page, pages, limit }: ProductsResponse = await fetch(
-    `${API_URL}/products/?_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
+    `${API_URL}/products?_page=${currentPage}&_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category`,
   ).then((res) => res.json());
-  //total instock products 
+  //total instock products
   const inStock = products.filter((product) => product.stock ?? 0 > 0).length;
-//total low stock products 
- const lowStock = products.filter((product) => (product.stock ?? 0) > 0 && (product.stock ?? 0) <= 10).length;
-//total out of stock products
-  const outOfStock = products.filter((product) => (product.stock ?? 0) === 0).length;
+  //total low stock products
+  const lowStock = products.filter(
+    (product) => (product.stock ?? 0) > 0 && (product.stock ?? 0) <= 10,
+  ).length;
+  //total out of stock products
+  const outOfStock = products.filter(
+    (product) => (product.stock ?? 0) === 0,
+  ).length;
 
   // console.log(products);
 
@@ -34,15 +47,15 @@ export default async function Home() {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <Header /> {/* calling the Header component to display the header of the page */}
-     {/* calling the SummaryCards component to display the summary cards of the page */}
-     <SummaryCards
+      <Header />{" "}
+      {/* calling the Header component to display the header of the page */}
+      {/* calling the SummaryCards component to display the summary cards of the page */}
+      <SummaryCards
         total={total}
         inStock={inStock}
         lowStock={lowStock}
         outOfStock={outOfStock}
       />
-
       <div>
         {products.map((product) => (
           <h2 key={product.id}>
@@ -50,7 +63,19 @@ export default async function Home() {
           </h2>
         ))}
       </div>
-      <div>{products.map((product) => <h2 key={product.id}>{product.title} - {product.category?.name}</h2>)}</div>
+      <div>
+        {products.map((product) => (
+          <h2 key={product.id}>
+            {product.title} - {product.category?.name}
+          </h2>
+        ))}
+      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={pages}
+        totalItems={total}
+        pageSize={limit}
+      />
     </main>
   );
 }
