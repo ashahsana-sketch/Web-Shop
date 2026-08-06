6e2b4ca36b5e079e6d9852e01076c293aee0d2f5
ägaren <ashahsana@gmail.com>
2026-08-06 15:26:11 +0200

    complete the summary card

--- app/components/Summary-card/SummaryCard.tsx
+++ app/components/Summary-card/SummaryCard.tsx
@@ -1,3 +1,4 @@
+import { Package, CircleCheck, TriangleAlert, CircleX } from "lucide-react";
 type SummaryCardsProps = {
   total: number;
   inStock: number;
@@ -11,36 +12,58 @@ export default function SummaryCards({
   lowStock,
   outOfStock,
 }: SummaryCardsProps) {
-  const cards = [
-    {
-      title: "Total Products",
-      value: total,
-      color: "text-violet-700",
-    },
-    {
-      title: "In Stock",
-      value: inStock,
-      color: "text-green-700",
-    },
-    {
-      title: "Low Stock",
-      value: lowStock,
-      color: "text-orange-500",
-    },
-    {
-      title: "Out of Stock",
-      value: outOfStock,
-      color: "text-red-700",
-    },
-  ];
 
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-6 p-6"> {cards.map((card) => (
-        <div key={card.title}className="bg-white rounded-lg shadow-md p-6">
-          <h3 className="text-gray-600 text-sm">{card.title} </h3>
-          <p className={`text-3xl font-bold mt-2 ${card.color}`}> {card.value} </p>
+const cards = [
+  {
+    title: "Total Products",
+    value: total,
+    color: "text-violet-700",
+    icon: <Package className="w-6 h-6 text-violet-700" />,
+  },
+  {
+    title: "In Stock",
+    value: inStock,
+    color: "text-green-700",
+    icon: <CircleCheck className="w-6 h-6 text-green-700" />,
+  },
+  {
+    title: "Low Stock",
+    value: lowStock,
+    color: "text-orange-500",
+    icon: <TriangleAlert className="w-6 h-6 text-orange-500" />,
+  },
+  {
+    title: "Out of Stock",
+    value: outOfStock,
+    color: "text-red-700",
+    icon: <CircleX className="w-6 h-6 text-red-700" />,
+  },
+];
+
+return (
+  <div className="grid grid-cols-2 md:grid-cols-4 gap-6 p-6">
+    {cards.map((card) => (
+      <div
+        key={card.title}
+        className="bg-white rounded-lg shadow-md p-6"
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <h3 className="text-gray-600 text-sm">
+              {card.title}
+            </h3>
+
+            <p className={`text-3xl font-bold mt-2 ${card.color}`}>
+              {card.value}
+            </p>
+          </div>
+
+          <div className="flex items-center">
+            {card.icon}
+          </div>
         </div>
-      ))}
-    </div>
-  );
+      </div>
+    ))}
+  </div>
+);
 }
